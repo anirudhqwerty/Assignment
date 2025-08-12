43d815ad6c7c2119f9a54dededd1b588f9b72a4f
anirudhqwerty <therealanirudhsharma@gmail.com>
2025-08-12 10:57:36 +0530

    assignment 2 question 1

---
+++ Assignment 2/first.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0, right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target){
+             return mid;
+        }
+        else if (arr[mid] > target) {
+            right = mid - 1;
+        }
+        else{
+            left = mid + 1;
+        } 
+    }
+    return -1;
+}
+
+int main() {
+    int arr[100],size;
+    cout<<"What is the size of the array? "<<endl;
+    cin>>size;
+    cout<<"What are the elements of the array?"<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int target;
+    cout << "What element do you want to find? "<<endl;
+    cin >> target;
+    int index = binarySearch(arr, size, target);
+    if (index != -1) cout << "Found at index " << index << endl;
+    else cout << "Not found" << endl;
+}
