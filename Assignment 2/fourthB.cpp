f380582c1b25b897667bfd6964ad0723fea26447
anirudhqwerty <therealanirudhsharma@gmail.com>
2025-08-12 11:22:15 +0530

    assignment 2 question 4 part (b)

---
+++ Assignment 2/fourthB.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter the string: ";
+    getline(cin, str);
+
+    int start = 0, end = str.length() - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+
+    cout << "Reversed string:  " << str << endl;
+}
